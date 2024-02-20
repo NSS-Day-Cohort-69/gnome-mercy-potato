bad105d5780d14c686206367656cb0d428eecc51
Lucas Bresson <pandaboy816@gmail.com>
2024-02-20 11:54:48 -0600

    Name Parity etc

--- src/scripts/PurposeFormField.js
+++ src/scripts/PurposeFormField.js
@@ -5,7 +5,7 @@ document.addEventListener("change", (event) => {
   }
 });
 
-export const makePurposeFormField = () => {
+export const PurposeFormField = () => {
   return `
     <label class="label" for="purpose">Purpose</label>
     <input placeholder="What should the effect be?" type="text" data-id="purpose" class="input">
