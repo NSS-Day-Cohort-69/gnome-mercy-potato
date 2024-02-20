30c958faacb6a2aa817249c67325c116c0b30837
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-20 12:06:32 -0600

    Updated render functions to include Craft Requests

--- src/scripts/GnomeMercy.js
+++ src/scripts/GnomeMercy.js
@@ -5,8 +5,16 @@
     components.
 */
 
-export const GnomeMercy = () => {
+import { CraftRequests } from "./CraftRequests.js";
+
+export const GnomeMercy = async () => {
+  const craftRequestsHTML = await CraftRequests()
+  
   return `
     <h1>Gnome Mercy</h1>
-    `;
+    <article>
+      <section class="section">
+        ${craftRequestsHTML}
+      </section>
+    </article>`;
 };
