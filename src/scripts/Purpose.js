303af1a29a144245fbf46075960e938f2d60e257
Lucas Bresson <pandaboy816@gmail.com>
2024-02-20 11:45:59 -0600

    Switched Input Id, into Data-id

--- src/scripts/Purpose.js
+++ src/scripts/Purpose.js
@@ -1,5 +1,5 @@
 document.addEventListener("change", (event) => {
-  if (event.target.id === "purpose") {
+  if (event.target.dataset.id === "purpose") {
     // Pretend the console.log, is our future transient edit.
     console.log(event.target.value);
   }
@@ -8,6 +8,6 @@ document.addEventListener("change", (event) => {
 export const makePurposeFormField = () => {
   return `
     <label class="label" for="purpose">Purpose</label>
-    <input placeholder="What should the effect be?" type="text" id="purpose" class="input">
+    <input placeholder="What should the effect be?" type="text" data-id="purpose" class="input">
     `;
 };
