32e85c93d0eb1c80bb44f217be673764be24faa9
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-02-20 15:57:27 -0600

    added html for submit button

--- src/scripts/CompleteButton.js
+++ src/scripts/CompleteButton.js
@@ -3,3 +3,12 @@
     Generate the HTML for the complete requests button,
     and POST a new item to the API when clicked.
 */
+//import { saveCraftSubmission } from "./dataAccess.js"
+//saveCraftSubmission
+
+export const CompleteButton = () =>
+{
+  let returnHTML = `<div class="complete-button">`
+  returnHTML += `<button class="complete-button--"></button>`
+  returnHTML += `</div>`
+}
